e68356aec5efbb08219eb9588091876c37278ff2
Adrian Lewandowski <alewandowski1708@gmail.com>
2025-10-11 21:33:04 +0200

    zad2b

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
-const data = fs.readFileSync('package.json');
 console.log("Początek skryptu.");
+fs.readFile('package.json', function (err, data) {
+if (err) return console.error(err);
 console.log(data.toString());
+});
 console.log("Koniec skryptu.");
+// w tym przypadku odczyt pliku jest asynchroniczny
+// (nie blokuje dalszego wykonywania skryptu)
