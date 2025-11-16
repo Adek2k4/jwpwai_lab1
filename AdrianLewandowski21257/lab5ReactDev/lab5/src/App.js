57e61ea6441272a393407d49915680a962ea5f24
Adrian Lewandowski <alewandowski1708@gmail.com>
2025-11-16 23:22:31 +0100

    lab5_zad2

--- AdrianLewandowski21257/lab5ReactDev/lab5/src/App.js
+++ AdrianLewandowski21257/lab5ReactDev/lab5/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import ToggleDetails from './ToggleDetails';
+import ScoreDisplay from './ScoreDisplay';
 
 function App() {
   return (
     <div className="App">
       <ToggleDetails />
+      <ScoreDisplay score={0} />
+      <ScoreDisplay score={75} />
+      <ScoreDisplay score={500} />
     </div>
   );
 }
